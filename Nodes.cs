d303f1d77773b8edc1e7ce2904031fddf0cffcba
tanvishnide <118621300+tanvishinde03@users.noreply.github.com>
2023-01-15 18:35:51 +0530

    Welcome To The Link List

---
+++ Nodes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListUCs
+{
+    internal class Nodes
+    {
+        internal Nodes next;
+        internal int data;
+
+        public Nodes(int data)
+        {
+            this.data = data;
+        }
+
+       
+        public class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
